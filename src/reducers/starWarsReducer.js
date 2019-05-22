160640ee5057d96cb41514fba22dfc7f8660507b
Sydney Crumley <srsimps19@gmail.com>
2019-05-22 15:00:52 -0500

    got map state to props plugged in

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -3,13 +3,10 @@ const initialState = {
   characters: [],
   fetching: false,
   error: ''
-  // Array characters, Boolean fetching, null error.
+  
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
     case FETCHING:
       return {
         ...state,
