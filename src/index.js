3dca56e4a5b5ba76f414c854de61aa82c6250b4a
yukaliv <yuka.okamoto0810@gmail.com>
2019-05-22 12:52:33 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { applyMiddleware} from 'redux';
+
 // needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+// applyMiddleware from redux ok
+// thunk from redux-thunk ok
+// logger from redux-logger ok
+// rootReducer from ./reducers ok
 
 const store = createStore(
-  rootReducer
+  rootReducer, applyMiddleware(thunk)
   /* applyMiddleware goes here */
 );
 
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,32 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH_STARWARS_START, FETCH_STARWARS_SUCCESS, FETCH_STARWARS_FAIL } from/* we need our action types here*/ "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  error: '',
+  isFetching: false
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_STARWARS_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: ''
+      };
+    case FETCH_STARWARS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: '',
+        characters: action.payload
+      };
+    case FETCH_STARWARS_FAIL:
+      return {
+        ...state,
+        error: action.payload
+      }
+
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
