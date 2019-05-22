990718a0ab2b77c8557ec8854c76b5999da6e702
Sydney Crumley <srsimps19@gmail.com>
2019-05-22 14:30:05 -0500

    first try at starWarsReducer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
